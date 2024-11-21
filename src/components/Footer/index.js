7427e9cddb5dbdf6ff9ba50afd0764bd036a518c
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-11-21 18:18:44 -0400

    feat: finish footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,84 @@
+import FacebookIcon from '@mui/icons-material/Facebook'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import YouTubeIcon from '@mui/icons-material/YouTube'
 import React from 'react'
 
-import { FooterContainer } from './styles'
+import {
+  FooterContainer,
+  FooterWrapper,
+  FooterInfo,
+  SocialLinks,
+  FooterContent,
+  FooterHeading,
+  FooterContact,
+  Credits
+} from './styles'
 
 export function Footer() {
   return (
     <FooterContainer>
-      <div className="direitos-autorais">
-        <h4 id="mudarcordireitos">@2024 LUCAS F. ALL RIGHTS RESERVED</h4>
+      <div className="container">
+        <FooterWrapper className="row gy-4">
+          <section>
+            <FooterInfo>
+              <a className="logo">
+                <img
+                  src="https://nibgrandecircular.com/themes/strata/assets/images/Marca_20anos.png"
+                  alt="NIB Grande Circular"
+                />
+              </a>
+              <SocialLinks>
+                <a
+                  href="https://www.facebook.com/NIBGrandeCircular"
+                  className="facebook"
+                >
+                  <FacebookIcon />
+                </a>
+                <a
+                  href="https://www.instagram.com/nibgrandecircular/"
+                  className="instagram"
+                >
+                  <InstagramIcon />
+                </a>
+                <a
+                  href="https://www.youtube.com/@NIBGrandeCircular"
+                  className="youtube"
+                >
+                  <YouTubeIcon />
+                </a>
+              </SocialLinks>
+            </FooterInfo>
+          </section>
+
+          <FooterContent>
+            <FooterHeading>Contato</FooterHeading>
+            <FooterContact>
+              <p>
+                Avenida Autaz Mirim, 7761 <br />
+                Tancredo Neves
+                <br />
+                Manaus - AM - Brasil <br />
+                <br />
+                <strong>Telefone: </strong>
+                <a
+                  href="https://wa.me/5592984590712"
+                  style={{ color: 'antiquewhite' }}
+                >
+                  +55 92 98459-0712
+                </a>
+                <br />
+                <strong>E-mail: </strong>secretaria@nibgrandecircular.com
+                <br />
+              </p>
+            </FooterContact>
+          </FooterContent>
+        </FooterWrapper>
+      </div>
+
+      <div className="container mt-4">
+        <Credits>
+          <h4 id="mudarcordireitos">@2024 LUCAS F. ALL RIGHTS RESERVED</h4>
+        </Credits>
       </div>
     </FooterContainer>
   )
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,12 +1,118 @@
 import styled from 'styled-components'
 
 export const FooterContainer = styled.footer`
-  margin-bottom: 20px;
-  .direitos-autorais {
-    text-align: center;
-    font-size: 12px;
-    &.active {
-      color: white;
+  box-sizing: border-box;
+  border: 0 solid #e5e7eb;
+  border-top-width: 1px;
+  border-color: rgb(33 38 45);
+  font-size: 14px;
+  background-color: var(--color-primary);
+  padding: 50px 0;
+  color: white;
+
+  .container {
+    @media (min-width: 576px) {
+      max-width: 900px;
     }
+
+    width: 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+`
+export const FooterWrapper = styled.div`
+  flex-wrap: wrap;
+
+  display: flex;
+  justify-content: space-between;
+`
+
+export const FooterInfo = styled.div`
+  .logo {
+    line-height: 0;
+    margin-bottom: 25px;
+
+    img {
+      max-height: 100px;
+      margin-right: 6px;
+    }
+
+    span {
+      font-size: 30px;
+      font-weight: 700;
+      letter-spacing: 1px;
+      color: #fff;
+      font-family: var(--font-primary);
+    }
+  }
+
+  p {
+    font-size: 14px;
+    font-family: var(--font-primary);
+  }
+`
+
+export const SocialLinks = styled.div`
+  display: flex;
+  margin-top: 16px;
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    border: 1px solid rgba(255, 255, 255, 0.2);
+    font-size: 16px;
+    color: rgba(255, 255, 255, 0.7);
+    margin-right: 10px;
+    transition: 0.3s;
+
+    &:hover {
+      color: #fff;
+      border-color: #fff;
+    }
+  }
+`
+export const FooterContent = styled.section`
+  text-align: center;
+  margin-top: 25px;
+  @media (min-width: 992px) {
+    -ms-flex: 0 0 25%;
+    flex: 0 0 25%;
+    max-width: 25%;
+  }
+  @media (min-width: 768px) {
+    max-width: 100%;
+  }
+`
+
+export const FooterHeading = styled.h4`
+  font-size: 16px;
+  font-weight: bold;
+  position: relative;
+  padding-bottom: 12px;
+`
+
+export const FooterContact = styled.div`
+  p {
+    line-height: 26px;
+  }
+`
+
+export const Credits = styled.div`
+  box-sizing: border-box;
+  border: 0 solid #e5e7eb;
+  border-top-width: 1px;
+  border-color: rgb(33 38 45);
+  padding-top: 4px;
+  text-align: center;
+  font-size: 13px;
+
+  a {
+    color: #fff;
   }
 `
