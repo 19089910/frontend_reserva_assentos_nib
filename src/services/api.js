a853500aaaee573f7a58f5c042bd697d2e90cd5b
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2025-01-21 19:44:43 -0400

    deploy: update service

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const apiNibAssento = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'http://backendreservaassentosnib-production.up.railway.app'
+  // 'http://localhost:3001'
 })
 export default apiNibAssento
