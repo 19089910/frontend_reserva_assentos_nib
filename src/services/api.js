6ac02547c42686c9f6d591e1cfbe98db064ba25c
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2025-01-21 20:23:21 -0400

    deploy: test cors https

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const apiNibAssento = axios.create({
-  baseURL: 'http://backendreservaassentosnib-production.up.railway.app'
+  baseURL: 'https://backendreservaassentosnib-production.up.railway.app'
   // 'http://localhost:3001'
 })
 export default apiNibAssento
